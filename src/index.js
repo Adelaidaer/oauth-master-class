4aa0c7523afa377ef4406522826080379049b55a
adelaider <117357197+Adelaidaer@users.noreply.github.com>
2025-11-15 14:39:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,5 +33,27 @@ window.onload = () => {
       .catch((error) => console.log("Обработка ошибки", error));
   };
   document.getElementById("button").onclick = () => {
-      };
-};
+     window.YaAuthSuggest.init(
+     {
+       client_id: "f8c0fb137ddc4bd2acea1a1489b5ae1b",
+       response_type: "token",
+       redirect_uri: "https://oauth-master-class.vercel.app/token2.html",
+     },
+       "https://oauth-master-class.vercel.app",
+     {
+       parentId: "buttonContainer",
+       view: "button",
+       buttonTheme: "light",
+       buttonSize: "xs",
+       buttonBorderRadius: 20,
+       }
+     )
+       .then(({ handler }) => handler())
+       .then(async (data) => {
+         console.log("Сообщение с токеном(от кнопки): ", data);
+         const result = await fetchYandexData(data.access_token);
+         console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
+         authorize(result);
+       })
+       .catch((error) => console.log("Что-то пошло не так: ", error));
+ };
