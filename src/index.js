6add260eb1e42dc079aa7220d6e57761bdf86882
adelaider <117357197+Adelaidaer@users.noreply.github.com>
2025-11-15 17:25:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ document.getElementById("button").onclick = () => {
      {
        client_id: "f7b7acf3673f41668b3ac33775ad56cf",
        response_type: "token",
-       redirect_uri: "https://oauth.yandex.ru/verification_code/token.html",
+       redirect_uri: "https://oauth-master-class-81jj.vercel.app/token.html",
      },
-     "https://oauth.yandex.ru/verification_code",
+     "https://oauth-master-class-81jj.vercel.app",
      {
        parentId: "buttonContainer",
        view: "button",
