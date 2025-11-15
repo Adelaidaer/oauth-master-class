f57fa57dfaf7c66192e13d82d4509ec30e88ac32
adelaider <117357197+Adelaidaer@users.noreply.github.com>
2025-11-15 14:13:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url(’https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle’)"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+  document.getElementById("auth").innerHTML = `${avatarHTML}${nameHTML}`;
+};
+
+const fetchYandexData = (token) =>
+  fetch().then(
+    (res) => res.json()
+);
+
 window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+   YaAuthSuggest.init(
+      {
   document.getElementById("button").onclick = () => {
     YaAuthSuggest.init(
       {
