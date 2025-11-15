3b53badba15e3261c523565c16c144f300f2e975
adelaider <117357197+Adelaidaer@users.noreply.github.com>
2025-11-15 15:31:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,25 +11,27 @@ const fetchYandexData = (token) =>
   fetch(`https://login.yandex.ru/info?format=json&auth_token=${token}`).then(
     (res) => res.json()
 );
-
 window.onload = () => {
   document.getElementById("button").onclick = () => {
-    YaAuthSuggest.init(
+    window.YaAuthSuggest.init(
       {
         client_id: "f7b7acf3673f41668b3ac33775ad56cf",
         response_type: "token",
         redirect_uri: "https://oauth-master-class-81jj.vercel.app/token.html",
       },
       "https://oauth-master-class-81jj.vercel.app",
+      {
+        view: "button",
+        parentId: "buttonContainer",
+        buttonSize: "m",
+        buttonView: "main",
+        buttonTheme: "light",
+        buttonBorderRadius: "0",
+        buttonIcon: "ya",
+      }
     )
       .then(({ handler }) => handler())
-      .then(async (data) => { 
-        const result = await fetchYandexData(data.access_token);
-
-        authorize(result);
-
-        console.log(result, data);
-      })
+      .then((data) => console.log("Сообщение с токеном", data))
       .catch((error) => console.log("Обработка ошибки", error));
   };
-
+};
